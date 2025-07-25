dc9b1e6bf6d16f43068308cd005496be4fb70a60
Reza Fouladian <9205977+rezafouladian@users.noreply.github.com>
2025-07-25 11:01:03 -0700

    Fix display

--- core/src/mac/portable/video.rs
+++ core/src/mac/portable/video.rs
@@ -17,8 +17,10 @@ where
     const DISPLAY_WIDTH: usize = 640;
     /// Display height in pixels
     const DISPLAY_HEIGHT: usize = 400;
+    
+    const DISPLAY_SIZE: usize = Self::DISPLAY_WIDTH * Self::DISPLAY_HEIGHT;
 
-    const FRAMEBUFFER_SIZE: usize = (Self::DISPLAY_WIDTH * Self::DISPLAY_HEIGHT) / 8;
+    const FRAMEBUFFER_SIZE: usize = ((Self::DISPLAY_WIDTH * Self::DISPLAY_HEIGHT) / 8) + 768;
 
     pub fn new(renderer: T) -> Self {
         Self {
@@ -33,6 +35,21 @@ where
 
         let buf = self.renderer.buffer_mut();
         buf.set_size(Self::DISPLAY_WIDTH, Self::DISPLAY_HEIGHT);
+        
+        for idx in 0..Self::DISPLAY_SIZE {
+            let byte = idx / 8;
+            let bit = idx % 8;
+            if fb[byte] & (1 << (7 - bit)) == 0 {
+                buf[idx * 4] = 0xEE;
+                buf[idx * 4 + 1] = 0xEE;
+                buf[idx * 4 + 2] = 0xEE;
+            } else {
+                buf[idx * 4] = 0x22;
+                buf[idx * 4 + 1] = 0x22;
+                buf[idx * 4 + 2] = 0x22;
+            }
+            buf[idx * 4 + 3] = 0xFF;
+        }
 
         self.renderer.update()?;
 
